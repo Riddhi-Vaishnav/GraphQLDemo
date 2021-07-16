d3b3036a1180b7158dbd8ec10ea3a7982e11d0ed
Riddhi-Vaishnav <74799578+Riddhi-Vaishnav@users.noreply.github.com>
2021-07-16 16:44:40 -0400

    Querying multiple lists

--- server/App.js
+++ server/App.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql').graphqlHTTP;
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
 
 
 const app = express();
+mongoose.connect('mongodb+srv://rvaishnav3010:Shdr3010@cluster0.zmypv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
+mongoose.connection.once('open', () => {
+    console.log('connected to Database');
+});
 
 app.use('/graphql', graphqlHTTP({
     schema,
