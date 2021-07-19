7a09048fd3f8f6dc3cee39cdcb3f821314730f8e
Riddhi-Vaishnav <74799578+Riddhi-Vaishnav@users.noreply.github.com>
2021-07-19 17:38:18 -0400

    Added Client_Side with React

--- server/App.js
+++ server/App.js
@@ -5,9 +5,14 @@ const mongoose = require('mongoose');
 
 
 const app = express();
-mongoose.connect('mongodb+srv://rvaishnav3010:Shdr3010@cluster0.zmypv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
+mongoose.connect('mongodb+srv://riddhivaishnav1130:Shdr3010@graphql.8nmvz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+{useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false});
+//mongoose.connect('mongodb+srv://rvaishnav3010:Shdr3010@cluster0.zmypv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
 mongoose.connection.once('open', () => {
-    console.log('connected to Mongo Database');
+    console.log('connected to  New Mongo Database');
 });
 
 app.use('/graphql', graphqlHTTP({
